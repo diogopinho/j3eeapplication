39512779b0dca235636a9badf6d60a6d1468dc37
diogopinho <diogo88pinho@gmail.com>
2018-11-20 16:38:27 +0000

    Part 17 completed

--- j2eeapplication/src/main/java/com/example/j2eeapp/services/impl/UserServiceImpl.java
+++ j2eeapplication/src/main/java/com/example/j2eeapp/services/impl/UserServiceImpl.java
@@ -1,5 +1,9 @@
 package com.example.j2eeapp.services.impl;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import com.example.j2eeapp.dao.UserDao;
 import com.example.j2eeapp.domain.UserEntity;
 import com.example.j2eeapp.services.UserService;
@@ -11,7 +15,7 @@ import com.example.j2eeapp.services.UserService;
  * @author Diogo
  *
  */
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService,UserDetailsService {
 
 	private UserDao userDao;
 	
@@ -26,6 +30,18 @@ public class UserServiceImpl implements UserService {
 		userDao.save(userEntity);
 		return true;
 	}
+	
+
+	/**
+	 * Construct UserDetails instance required by Spring security.
+	 */
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		
+		return null;
+	}
+
+
 
 	public UserDao getUserDao() {
 		return userDao;
