20f2aa5c01f965dd44866d4fdfebe5a1f6c78265
diogopinho <diogo88pinho@gmail.com>
2018-11-21 08:39:14 +0000

    Part 17 completed

--- j2eeapplication/src/main/java/com/example/j2eeapp/services/impl/UserServiceImpl.java
+++ j2eeapplication/src/main/java/com/example/j2eeapp/services/impl/UserServiceImpl.java
@@ -1,5 +1,11 @@
 package com.example.j2eeapp.services.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -37,8 +43,15 @@ public class UserServiceImpl implements UserService,UserDetailsService {
 	 */
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
+		UserEntity user = userDao.loadUserByUserName(username);
 		
-		return null;
+		if(user==null) {
+			throw new UsernameNotFoundException(String.format("No such user with name provided '%s'", username));
+		}
+		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+		User userDetails = new User(user.getUserName(), user.getPassword(), authorities);
+		return userDetails;
 	}
 
 
