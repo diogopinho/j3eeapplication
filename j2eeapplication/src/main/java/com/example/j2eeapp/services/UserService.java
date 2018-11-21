7b1c5e3035d1dbb917e46414b4bd1c18b81bb345
diogopinho <diogo88pinho@gmail.com>
2018-11-21 12:50:04 +0000

    Completed Arthur project.

--- j2eeapplication/src/main/java/com/example/j2eeapp/services/UserService.java
+++ j2eeapplication/src/main/java/com/example/j2eeapp/services/UserService.java
@@ -1,5 +1,7 @@
 package com.example.j2eeapp.services;
 
+import javax.faces.event.AjaxBehaviorEvent;
+
 import com.example.j2eeapp.domain.UserEntity;
 
 /**
@@ -19,4 +21,20 @@ public interface UserService {
 	 * @return
 	 */
 	boolean createUser(UserEntity userEntity);
+	
+	/**
+	 * Check user name availability. UI Ajax use.
+	 * 
+	 * @param event
+	 * @return
+	 */
+	boolean checkAvailable(AjaxBehaviorEvent event);
+	
+	/**
+	 * Retrieves full user record from database by user name.
+	 * 
+	 * @param userName
+	 * @return
+	 */
+	UserEntity loadUserEntityByUsername(String userName);
 }
--- j2eeapplication/src/main/java/com/example/j2eeapp/services/impl/UserServiceImpl.java
+++ j2eeapplication/src/main/java/com/example/j2eeapp/services/impl/UserServiceImpl.java
@@ -2,10 +2,13 @@ package com.example.j2eeapp.services.impl;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.ResourceBundle;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
 
+import org.primefaces.component.inputtext.InputText;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -38,7 +41,7 @@ public class UserServiceImpl implements UserService,UserDetailsService {
 	public boolean createUser(UserEntity userEntity) {
 		
 		if(!userDao.checkAvailable(userEntity.getUserName())) {
-			FacesMessage message = constructErrorMessage(String.format("User Name '%s' is not available", userEntity.getUserName()),null);
+			FacesMessage message = constructErrorMessage(String.format(getMessageBundle().getString("userExistsMsg"), userEntity.getUserName()),null);
 			getFacesContext().addMessage(null, message);
 			return false;
 		}
@@ -53,6 +56,27 @@ public class UserServiceImpl implements UserService,UserDetailsService {
 		return true;
 	}
 	
+	/**
+	 * Check user name availability. UI Ajax use.
+	 * 
+	 * @param event
+	 * @return
+	 */
+	public boolean checkAvailable(AjaxBehaviorEvent event) {
+		
+		InputText inputText = (InputText) event.getSource();
+		String value = (String) inputText.getValue();
+		
+		boolean available = userDao.checkAvailable(value);
+		if(!available) {
+			FacesMessage message = constructErrorMessage(null, String.format(getMessageBundle().getString("userExistsMsg"), value));
+			getFacesContext().addMessage(event.getComponent().getClientId(), message);
+		} else {
+			FacesMessage message = constructInfoMessage(null, String.format(getMessageBundle().getString("userAvailableMsg"), value));
+			getFacesContext().addMessage(event.getComponent().getClientId(), message);
+		}
+		return available;
+	}
 
 	/**
 	 * Construct UserDetails instance required by Spring security.
@@ -62,7 +86,7 @@ public class UserServiceImpl implements UserService,UserDetailsService {
 		UserEntity user = userDao.loadUserByUserName(username);
 		
 		if(user==null) {
-			throw new UsernameNotFoundException(String.format("No such user with name provided '%s'", username));
+			throw new UsernameNotFoundException(String.format(getMessageBundle().getString("badCredentials"), username));
 		}
 		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
@@ -70,6 +94,15 @@ public class UserServiceImpl implements UserService,UserDetailsService {
 		return userDetails;
 	}
 
+	/**
+	 * Retrieves full user record from database by user name.
+	 * 
+	 * @param userName
+	 * @return
+	 */
+	public UserEntity loadUserEntityByUsername(String userName) {
+		return userDao.loadUserByUserName(userName);
+	}
 
 	protected FacesMessage constructErrorMessage(String message, String detail) {
 		return new FacesMessage(FacesMessage.SEVERITY_ERROR,message,detail);
@@ -85,6 +118,10 @@ public class UserServiceImpl implements UserService,UserDetailsService {
 	
 	protected FacesContext getFacesContext() {
 		return FacesContext.getCurrentInstance();
+	}
+	
+	protected ResourceBundle getMessageBundle() {
+		return ResourceBundle.getBundle("message-labels");
 	}
 
 	public UserDao getUserDao() {
